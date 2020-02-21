4559fc4b7502e7fc033a2357a7694449325c7025
addzycullen <adam.cullen@live.co.uk>
2020-02-21 22:00:19 +0000

    Added SVG Icon example to Account Page Block

--- src/blocks/block-account-page/index.js
+++ src/blocks/block-account-page/index.js
@@ -6,16 +6,17 @@ const { __ } = wp.i18n;
 const { registerBlockType } = wp.blocks;
 
 // Import block editor settings
-import EditBlock from './edit';
+import EditBlock from "./edit";
+import icon from "./icon";
 
-let blockName = 'account-page';
+let blockName = "account-page";
 
 // Register the block.
-registerBlockType( 'wpum/' + blockName, {
+registerBlockType("wpum/" + blockName, {
 	title: wpum_blocks.blocks[blockName].labels.title,
 	description: wpum_blocks.blocks[blockName].labels.description,
-	icon: wpum_blocks.blocks[blockName].labels.icon,
-	category: 'wpum',
+	icon,
+	category: "wpum",
 	keywords: wpum_blocks.blocks[blockName].labels.keywords,
 	attributes: wpum_blocks.blocks[blockName].attributes,
 
